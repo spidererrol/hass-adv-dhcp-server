0888b437135bf304237c39167b72f05322b29878
Timothy Hinchcliffe <gitprojects.qm@spidererrol.co.uk>
2024-11-16 21:41:34 +0000

    Remove debugging output.

--- adv-dhcp-server/run.sh
+++ adv-dhcp-server/run.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bashio
 
-set -x
-
 shopt -s nullglob
 
-ls -hal /
-readlink -f /etc/dhcp/
-ls -hal /etc/dhcp/
-readlink -f /var/lib/dhcp/
-ls -hal /var/lib/dhcp/
-
-
 if ! test -L /etc/dhcp; then
-    mv -n /etc/dhcp/* /config/
+    mv -n /etc/dhcp/* /config/ || :
     rm -rf /etc/dhcp
     ln -s /config/ /etc/dhcp
 fi
 
 if ! test -L /var/lib/dhcp; then
-    mv -n /var/lib/dhcp/* /data/
+    mv -n /var/lib/dhcp/* /data/ || :
     rm -rf /var/lib/dhcp
     ln -s /data/ /var/lib/dhcp
 fi
