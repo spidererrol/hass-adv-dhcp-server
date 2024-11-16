ffa43320c01fd0b918ca1fe9b7a16946f3d2b513
Timothy Hinchcliffe <gitprojects.qm@spidererrol.co.uk>
2024-11-16 21:31:28 +0000

    Debugging

--- adv-dhcp-server/run.sh
+++ adv-dhcp-server/run.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bashio
 
+set -x
+
 shopt -s nullglob
+
+ls -hal /
+readlink -f /etc/dhcp/
+ls -hal /etc/dhcp/
+readlink -f /var/lib/dhcp/
+ls -hal /var/lib/dhcp/
+
 
 if ! test -L /etc/dhcp; then
     mv -n /etc/dhcp/* /addon_config/
